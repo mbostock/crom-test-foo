a7ed8c7d0018e66a8169a320965cde97dd5691da
Mike Bostock <mbostock@gmail.com>
2016-01-26 15:33:51 -0800

    Better tests.

--- foo.js
+++ foo.js
@@ -1,4 +1,7 @@
 var crom = require("crom"),
     format = crom.require("d3-format");
 
-console.log(format.format(",d")(1234));
+console.log((format.format(",d")(1234) === "1,234" ? "✓" : "✗") + " d3-format");
+console.log((format === crom.require("d3-format@0.5") ? "✓" : "✗") + " d3-format@0.5");
+console.log((format === crom.require("d3/d3-format") ? "✓" : "✗") + " d3/d3-format");
+console.log((format === crom.require("https://github.com/d3/d3-format") ? "✓" : "✗") + " https://github.com/d3/d3-format");
